3133d7cc5c3727799c8600c94e3bdfe0873694d4
Diego López Bugna <diegolopezbugna@gmail.com>
2018-04-18 10:00:14 -0300

    Add UserCollectionViewCell

---
+++ Random User Example/Random User Example/UserCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserCollectionViewCell.swift
+//  Random User Example
+//
+//  Created by Diego López Bugna on 18/04/2018.
+//  Copyright © 2018 Diego López Bugna. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- Random User Example/Random User Example/UsersCollectionViewController.swift
+++ Random User Example/Random User Example/UsersCollectionViewController.swift
@@ -14,19 +14,10 @@ class UsersCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     /*
@@ -43,19 +34,20 @@ class UsersCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! UserCollectionViewCell
     
         // Configure the cell
+        cell.imageView.backgroundColor = .black
     
         return cell
     }
